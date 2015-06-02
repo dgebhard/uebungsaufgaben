e05a5e04908283acc664db1b59e59bd64f1b673b
Dominik Gebhard <dominik.gebhard@competec.ch>
2015-06-02 13:20:56 +0200

    Fehler korrigiert

--- src/Person.php
+++ src/Person.php
@@ -1,23 +1,62 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: dev
- * Date: 6/2/15
- * Time: 9:21 AM
- */
 
-class Person {
+class Person
+{
+    /**
+     * @var string
+     */
     private $fname;
+
+    /**
+     * @var string
+     */
     private $lname;
+
+    /**
+     * @var string
+     */
     private $address;
+
+    /**
+     * @var int
+     */
     private $zip;
+
+    /**
+     * @var string
+     */
     private $place;
+
+    /**
+     * @var string
+     */
     private $birthdate;
+
+    /**
+     * @var string
+     */
     private $favcolor;
+
+    /**
+     * @var string
+     */
     private $phone;
 
+    /**
+     * @var array
+     */
     private $alreadyAsked;
 
+    /**
+     * @param string $fname
+     * @param string $lname
+     * @param string $address
+     * @param int $zip
+     * @param string $place
+     * @param string $birthdate
+     * @param string $phone
+     * @param string $favcolor
+     */
     function Person($fname, $lname, $address, $zip, $place, $birthdate, $phone, $favcolor)
     {
         $this->fname = $fname;
@@ -33,6 +72,9 @@ class Person {
         $this->alreadyAsked = array_fill_keys($keys, false);
     }
 
+    /**
+     * @return string
+     */
     public function getFullName()
     {
         $this->alreadyAsked['fname'] = true;
@@ -40,35 +82,59 @@ class Person {
         return $this->fname." ".$this->lname;
     }
 
+    /**
+     * @return string
+     */
     public function getPlace()
     {
         $this->alreadyAsked['place'] = true;
         return $this->place;
     }
 
+    /**
+     * @return string
+     */
     public function getBirthdate()
     {
         $this->alreadyAsked['birthdate'] = true;
         return $this->birthdate;
     }
 
+    /**
+     * @return string
+     */
     public function getPhoneNumber()
     {
         $this->alreadyAsked['phone'] = true;
         return $this->phone;
     }
 
+    /**
+     * @param $var
+     *
+     * @return mixed
+     */
     public function getAlreadyAsked($var)
     {
-        return $this->alreadyAsked[$var];
+        if (array_key_exists($var, $this->alreadyAsked)) {
+            return $this->alreadyAsked[$var];
+        } else {
+            return "key does not exist";
+        }
     }
 
+    /**
+     * @return string
+     */
     public function getFavColor()
     {
         $this->alreadyAsked['favcolor'] = true;
         return $this->favcolor;
     }
 
+    /**
+     * @return string
+     */
     public function getAddress()
     {
         return $this->address;
--- tests/PersonTest.php
+++ tests/PersonTest.php
@@ -15,7 +15,16 @@ class PersonTest extends PHPUnit_Framework_TestCase
 
     public function setup()
     {
-        $this->person = new Person('Dominik','Gebhard','Altstr. 3','5103','Möriken','02.02.1997','252523552434','blau');
+        $this->person = new Person(
+            'Dominik',
+            'Gebhard',
+            'Altstr. 3',
+            '5103',
+            'Möriken',
+            '02.02.1997',
+            '252523552434',
+            'blau'
+        );
     }
 
     public function testDummy()
@@ -23,29 +32,38 @@ class PersonTest extends PHPUnit_Framework_TestCase
         $this->assertInstanceOf(Person::class, $this->person);
     }
 
-    public function testGetFullName(){
-        $this->assertEquals($this->person->getFullName(),'Dominik Gebhard');
+    public function testGetFullName()
+    {
+        $this->assertEquals($this->person->getFullName(), 'Dominik Gebhard');
     }
 
-    public function testGetPlace(){
-        $this->assertNotEquals($this->person->getPlace(),"Wildegg");
+    public function testGetPlace()
+    {
+        $this->assertEquals($this->person->getPlace(), 'Möriken');
+        $this->assertNotEquals($this->person->getPlace(), 'Wildegg');
     }
 
-    public function testGetAddress(){
-        $this->assertEquals($this->person->getAddress(),'Altstr. 3');
+    public function testGetAddress()
+    {
+        $this->assertEquals($this->person->getAddress(), 'Altstr. 3');
+        $this->assertNotEquals($this->person->getAddress(), 'Altstr3');
     }
 
-    public function testGetBirthdate(){
-        $this->assertEquals($this->person->getBirthdate(),'02.02.1997');
+    public function testGetBirthdate()
+    {
+        $this->assertEquals($this->person->getBirthdate(), '02.02.1997');
     }
 
-    public function testGetPhoneNumber(){
-        $this->assertEquals($this->person->getPhoneNumber(),'252523552434');
+    public function testGetPhoneNumber()
+    {
+        $this->assertEquals($this->person->getPhoneNumber(), '252523552434');
     }
 
-    public function testAlreadyAsked(){
+    public function testAlreadyAsked()
+    {
         $this->person->getFavColor();
         $this->assertTrue($this->person->getAlreadyAsked('favcolor'));
         $this->assertFalse($this->person->getAlreadyAsked('phone'));
+        $this->assertEquals($this->person->getAlreadyAsked('dfaf'), 'Key does not exist');
     }
 }
